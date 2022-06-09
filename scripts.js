cd17145436cbc6d9f258e55c803d9f4be6242da1
griffin-a <griffinalmquist@gmail.com>
2022-06-09 17:16:04 +1200

    init

--- scripts.js
+++ scripts.js
@@ -1,23 +1,35 @@
 const modal = document.getElementsByClassName("modal")[0];
 const registerButton = document.getElementById("trigger-modal");
 const closeButton = document.getElementsByClassName("close")[0];
+const submitButton = document.getElementById("form-submit");
 
-console.log(registerButton);
 
 registerButton.onclick = () => {
-    console.log("Clicked register!");
-    registerButton.className = "active";
-    modal.style.display = "block";
+    openModal();
 };
 
 closeButton.onclick = () => {
-    registerButton.className = "inactive";
-    modal.style.display = "none";
+    closeModal();
 };
+
+submitButton.onclick = () => {
+    closeModal();
+}
 
 window.onclick = event => {
     if (event.target == modal) {
         registerButton.className = "inactive";
         modal.style.display = "none";
     }
+}
+
+const closeModal = () => {
+    registerButton.className = "inactive";
+    modal.style.display = "none";
+}
+
+const openModal = () => {
+    console.log("Clicked register!");
+    registerButton.className = "active";
+    modal.style.display = "block";
 }
